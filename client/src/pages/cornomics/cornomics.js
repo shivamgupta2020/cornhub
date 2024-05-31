11761f1734b1acd0ccdd37862048772829d4b145
shivamgupta2020 <shivam.gupta1968@gmail.com>
2024-05-31 13:20:14 +0530

    updated footer text & cornomics

--- client/src/pages/cornomics/cornomics.js
+++ client/src/pages/cornomics/cornomics.js
@@ -4,17 +4,20 @@ import {
   Chart as ChartJS,
   ArcElement,
   Tooltip,
-  Legend
+  Legend,
+  plugins
 } from 'chart.js'
 
-import {Pie} from 'react-chartjs-2'
+import { Pie } from 'react-chartjs-2'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.defaults.color = '#f90';
+
 
 function Cornnomics() {
   const data = {
     labels: ['Presale', 'LP', 'Community Airdrops', 'Operations', 'Team- A Big Fat, Zero!'],
-    datasets:[
+    datasets: [
       {
         data: [48, 35, 12, 5, 0],
         backgroundColor: ['red', 'blue', 'green', 'yellow', 'purple']
@@ -22,14 +25,45 @@ function Cornnomics() {
     ]
   }
   const options = {
+    elements: {
+      arc: {
+        borderWidth: 1,
+        borderColor: 'black'
+      }
+    },
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+    // plugins: {
+    //   legend: {
+    //     labels: {
+    //       // This more specific font property overrides the global property
+    //       font: {
+    //         size: 14,
+    //         family: 'Skribble'
 
+    //       }
+    //     }
+    //   }
+    // }
   }
   return (
     <div>
       <div className="cornomics">
+        <h1 style={{textAlign:"center", color:"#f90"}}>Cornomics</h1>
         <div className='cornomics-container'>
-          <Pie data={data} options={options}>
-          </Pie>
+            <div className="piechart"><Pie data={data} options={options}></Pie></div>
+            <div className="labels">
+              <ul>
+                <li style={{color:"red"}}>Presale: 48%</li>
+                <li style={{color:"blue"}}>LP: 35%</li>
+                <li style={{color:"green"}}>Community Airdrops: 12%</li>
+                <li style={{color:"yellow"}}>Operations: 5%</li>
+                <li style={{color:"purple"}}>Team- A Big Fat, Zero!</li>
+              </ul>
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -73,6 +73,9 @@ function Home() {
                             ))}
                         {isModalOpen && <Modal video={currentVideo} onClose={closeModal} updateLikes={updateVideoLikes} />}
                     </div>
+                    <div className="footer-text">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </div>
                 </div>
             </div>
         </div>
@@ -138,8 +141,8 @@ const Modal = ({ onClose, video, updateLikes }) => {
                             <img src={require('./like_unfill.png')} style={{ width: "30px", height: "30px" }} id='like_button' />
                             <p id='like'>{`Like (${likes})`}</p>
                         </div>
-                        <div className="line2-right-bottom" 
-                            onMouseEnter={() => setShowTooltip(true)} 
+                        <div className="line2-right-bottom"
+                            onMouseEnter={() => setShowTooltip(true)}
                             onMouseLeave={() => setShowTooltip(false)}>
                             <p style={{ textAlign: "end" }}>Tip Creator</p>
                             {showTooltip && (
